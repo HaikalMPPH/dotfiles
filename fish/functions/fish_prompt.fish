c234705968a0ff1089e74c37d091fcc1a9b68548
HaikalMPPH <haikal.mpph65@gmail.com>
2023-07-06 19:04:38 +0700

    changed the fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,8 +1,8 @@
 function fish_prompt
-  set usrnm (set_color cyan; echo '[';  whoami; echo ']')
-  set dir   (set_color blue; echo '[';  pwd; echo ']')
-  set bat   (set_color magenta; echo '['; cat /sys/class/power_supply/BAT1/capacity; echo ']')
-
-  echo $bat $usrnm $dir
-  echo '> $ '
+  set usrnm (whoami)
+  set dir   (pwd)
+  set bat   (echo (cat /sys/class/power_supply/BAT1/capacity)%)
+  
+  echo -e "$(set_color green)$(printf '\u250f')$(set_color -o magenta)($bat)$(set_color -o cyan)($usrnm)$(set_color -o blue)($dir)"
+  echo "$(set_color -o green)$(printf '\u2517\u2501\u2501') \$ "
 end
