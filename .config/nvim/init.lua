181332daaa62ecc393a4b73f767a19a5618f7303
HaikalMPPH <haikal.mpph65@gmail.com>
2024-06-16 00:01:24 +0700

    alacritty migrate to toml

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,7 +33,7 @@ require("lazy").setup({
   'hrsh7th/nvim-cmp'                           ,
   'NLKNguyen/papercolor-theme'                 ,
   'saadparwaiz1/cmp_luasnip' ,
-  --'vim-scripts/AutoComplPop'                   ,
+  'vim-scripts/AutoComplPop'                   ,
   "L3MON4D3/LuaSnip",
   'godlygeek/tabular'                          ,
   'Lokaltog/vim-powerline'
@@ -81,60 +81,60 @@ vim.keymap.set('n' , '<leader>fg' , builtin.live_grep  , {})
 vim.keymap.set('n' , '<leader>fb' , builtin.buffers    , {})
 vim.keymap.set('n' , '<leader>fh' , builtin.help_tags  , {})
 
--- Add additional capabilities supported by nvim-cmp
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+---- Add additional capabilities supported by nvim-cmp
+--local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local lspconfig = require('lspconfig')
+--local lspconfig = require('lspconfig')
 
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'tsserver', 'rust_analyzer', 'zls' }
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    -- on_attach = my_custom_on_attach,
-    capabilities = capabilities,
-  }
-end
+---- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+--local servers = { 'clangd', 'tsserver', 'rust_analyzer', 'zls' }
+--for _, lsp in ipairs(servers) do
+--  lspconfig[lsp].setup {
+--    -- on_attach = my_custom_on_attach,
+--    capabilities = capabilities,
+--  }
+--end
 
--- luasnip setup
-local luasnip = require 'luasnip'
+---- luasnip setup
+--local luasnip = require 'luasnip'
 
--- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  }),
-}
+---- nvim-cmp setup
+--local cmp = require 'cmp'
+--cmp.setup {
+--  snippet = {
+--    expand = function(args)
+--      luasnip.lsp_expand(args.body)
+--    end,
+--  },
+--  mapping = cmp.mapping.preset.insert({
+--    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+--    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--    ['<C-Space>'] = cmp.mapping.complete(),
+--    ['<CR>'] = cmp.mapping.confirm {
+--      behavior = cmp.ConfirmBehavior.Replace,
+--      select = true,
+--    },
+--    ['<Tab>'] = cmp.mapping(function(fallback)
+--      if cmp.visible() then
+--        cmp.select_next_item()
+--      elseif luasnip.expand_or_jumpable() then
+--        luasnip.expand_or_jump()
+--      else
+--        fallback()
+--      end
+--    end, { 'i', 's' }),
+--    ['<S-Tab>'] = cmp.mapping(function(fallback)
+--      if cmp.visible() then
+--        cmp.select_prev_item()
+--      elseif luasnip.jumpable(-1) then
+--        luasnip.jump(-1)
+--      else
+--        fallback()
+--      end
+--    end, { 'i', 's' }),
+--  }),
+--  sources = cmp.config.sources({
+--    { name = 'nvim_lsp' },
+--    { name = 'luasnip' },
+--  }),
+--}
